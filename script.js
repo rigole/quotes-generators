c1822db9865d86d5d0da1c54e996d75a9b36d5cc
rigole <foplacide@gmail.com>
2021-06-01 19:36:32 +0100

    fetching data using Javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get quote from API
+async function getQuote() {
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try{
+        
+    }catch (e) {
+        console.log('Oops No Quote', e);
+    }
+}
+
+getQuote();
