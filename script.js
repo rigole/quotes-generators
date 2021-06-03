e898f7e9b9b2d420c6c5ce6c498f41ab8011871a
rigole <foplacide@gmail.com>
2021-06-03 21:41:14 +0100

    access to API quote problem solved

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // Get quote from API
-async function getQuote() {
-    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
+
+
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
-        
+       // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const response = await fetch(proxyUrl+apiUrl);
+        const data = await response.json();
+        authorText.innerText = data.quoteAuthor;
+        quoteText.innerText = data.quoteText;
     }catch (e) {
+        getQuote();
         console.log('Oops No Quote', e);
     }
 }
