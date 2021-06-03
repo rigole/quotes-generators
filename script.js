81740ae822afc314f9766b6effadc78f957632e9
rigole <foplacide@gmail.com>
2021-06-03 21:52:59 +0100

    case of no author and long quote management

--- script.js
+++ script.js
@@ -11,9 +11,22 @@ async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
-       // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
         const response = await fetch(proxyUrl+apiUrl);
         const data = await response.json();
+        // if there are no longer author, put unknown
+        if (data.quoteAuthor === 'Unknown'){
+            authorText.innerText = 'Unknown';
+        }else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // reduce size for long quote
+        if (data.quoteText.length > 50){
+            quoteText.classList.add('long-quote');
+        }else
+        {
+            quoteText.classList.remove('long-quote');
+        }
         authorText.innerText = data.quoteAuthor;
         quoteText.innerText = data.quoteText;
     }catch (e) {
